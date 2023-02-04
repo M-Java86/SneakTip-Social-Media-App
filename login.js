5549ef106913955a8e1e4a75a1bd3bfcf09dcda7
M-Java86 <mpythondata@outlook.com>
2023-02-04 01:45:14 -0500

    modified user

--- login.js
+++ login.js
@@ -1,33 +1,77 @@
-//Target Nodes//
+//Targeting nodes
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
-//Firebase-for every api call we need to out a URL &endpoint
 const USERS_URL =
-  'https://famous-phalanx-338605-default-rtdb.firebaseio.com/users.json';
+  'https://famous-phalanx-338605-default-rtdb.firebaseio.com/users';
 const POSTS_URL =
   'https://famous-phalanx-338605-default-rtdb.firebaseio.com/posts';
 const EXT = '.json';
 
-console.log(password, username);
+console.log(location);
 
 const user = {
   username: 'michelle8639am',
   name: 'michelle',
 };
-//setting a variable validateUser that's taking in user, creating a new promise with(re-resolve & rej-reject)
+
 const validateUser = (user) =>
   new Promise((res, rej) => {
     setTimeout(() => {
       res(JSON.stringify({ user, status: 200, ok: true }));
     }, 2000);
   });
-//fetch Api-provides interfaces for fetching resources.
-const getUser = (user) => fetch(`${USERS_URL}/{user.username}${EXT}`);
+
+const getUser = (user) => fetch(`${USERS_URL}/${user.username}${EXT}`);
 const postUser = (user) =>
   fetch(`${USERS_URL}${EXT}`, {
     method: 'post',
     headers: {
-      'content-type': 'application/json', //this application is expected to be json.
+      'content-type': 'application/json',
     },
+    body: JSON.stringify(user),
   });
+
+function browserValidation() {
+  if (!password.value || password.value.length < 5) {
+    return 'password failed';
+  }
+  return null;
+}
+
+const LoginBtn = document.getElementById('LoginBtn');
+LoginBtn.addEventListener('click', handleLoginBtn);
+
+async function handleLoginBtn(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const user = {
+    username: username.value,
+    password: password.value,
+  };
+  try {
+    const validationError = browserValidation();
+    if (validationError) {
+      throw Error(validationError);
+    }
+    const checkToSeeIfUserExistsInDatabase = await getUser(user);
+    if (!checkToSeeIfUserExistsInDatabase.ok) {
+      throw Error('Error validating the user');
+    }
+    const userInformationInDatabase =
+      await checkToSeeIfUserExistsInDatabase.json();
+    if (!userInformationInDatabase) {
+      throw Error("Username Doesn't exist");
+    }
+    if (userInformationInDatabase) {
+      localStorage.setItem(
+        'userInfo',
+        JSON.stringify(userInformationInDatabase)
+      );
+      window.location.href = 'dashboard.html';
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
